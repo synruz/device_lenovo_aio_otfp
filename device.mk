b518067879dd21ceb5805821aecd250e5a387430
Ninad Patil <ninadpatil100@gmail.com>
2017-07-03 22:02:27 +0530

    Added KA,SNap and something special and update kernel

--- device.mk
+++ device.mk
@@ -121,6 +121,12 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
 
+#PixlernBlitz_Extra
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/priv-app/K3:system/priv-app/K3
+    $(LOCAL_PATH)/prebuilt/priv-app/KAdiutor:system/priv-app/KAdiutor
+    $(LOCAL_PATH)/prebuilt/priv-app/Snap:system/priv-app/Snap
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/audio_device.xml:system/etc/audio_device.xml \
